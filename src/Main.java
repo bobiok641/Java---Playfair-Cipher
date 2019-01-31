ded73422580be6bfeb3f6d971085d4d05f8a6b0e
bojidar stanchev <bobiok641@gmail.com>
2019-01-31 18:53:24 +0200

    made keyword transformator method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        String keyWord = new String();
+
         System.out.println("Enter a keyword:");
-        String keyWord = input.next();
-
-
+        String keyWordFromInput = input.next();
+        keyWord = transformKeyWord(keyWordFromInput);
+        System.out.println(keyWord);
+        
         System.out.println("Enter a message to encrypt:");
         String message = input.next();
         if (message.length() % 2 == 0){
@@ -14,5 +19,26 @@ public class Main {
         }else{
             System.out.println("The message should contain even number of letters!");
         }
+    }
+
+    public static String transformKeyWord(String keyFromInput){
+        String keyTransformed = new String();
+        boolean letterIsUsed = false;
+        keyTransformed = keyTransformed + keyFromInput.charAt(0);
+
+        for (int i = 1; i < keyFromInput.length(); i++)
+        {
+            for (int j = 0; j < keyTransformed.length(); j++)
+            {
+                if (keyFromInput.charAt(i) == keyTransformed.charAt(j))
+                {
+                    letterIsUsed = true;
+                }
+            }
+            if (letterIsUsed == false)
+                keyTransformed = keyTransformed + keyFromInput.charAt(i);
+            letterIsUsed = false;
+        }
+        return keyTransformed;
     }
 }
