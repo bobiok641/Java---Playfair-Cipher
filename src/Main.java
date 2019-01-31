23d2e7c69e5b1d71c3d81f07ac3f1ca37e89ff5a
bojidar stanchev <bobiok641@gmail.com>
2019-01-31 20:05:22 +0200

    Made matrix generator method

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        char[][] matrix = new char[5][5];
 
         String keyWord = new String();
 
@@ -11,7 +12,8 @@ public class Main {
         String keyWordFromInput = input.next();
         keyWord = transformKeyWord(keyWordFromInput);
         System.out.println(keyWord);
-        
+        matrixGenerator(keyWord);
+
         System.out.println("Enter a message to encrypt:");
         String message = input.next();
         if (message.length() % 2 == 0){
@@ -40,5 +42,52 @@ public class Main {
             letterIsUsed = false;
         }
         return keyTransformed;
+    }
+
+    public static char[][] matrixGenerator(String KeyWord) {
+        char matrix[][] = new char[5][5];
+
+
+        boolean letterIsNotUsed = true;
+        char currentLetterToAdd;
+        String Key = KeyWord;
+        for (int i = 0; i < 26; i++)
+        {
+            currentLetterToAdd = (char) (i + 97);
+            if (currentLetterToAdd == 'j')
+                continue;
+            for (int j = 0; j < KeyWord.length(); j++)
+            {
+                if (currentLetterToAdd == KeyWord.charAt(j))
+                {
+                    letterIsNotUsed = false;
+                    break;
+                }
+            }
+            if (letterIsNotUsed)
+                Key = Key + currentLetterToAdd;
+            letterIsNotUsed = true;
+        }
+        System.out.println(Key);
+
+        int indexFromKey = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                matrix[i][j] = Key.charAt(indexFromKey);
+                indexFromKey++;
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(char[][] matrix){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
