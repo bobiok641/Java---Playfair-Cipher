1b6ec7bf751b1a0c1f52f3ac5ffa73c3ade191b8
bojidar stanchev <bobiok641@gmail.com>
2019-01-31 16:44:55 +0200

    made input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a keyword:");
+        String keyWord = input.next();
+
+
+        System.out.println("Enter a message to encrypt:");
+        String message = input.next();
+        if (message.length() % 2 == 0){
+
+        }else{
+            System.out.println("The message should contain even number of letters!");
+        }
     }
 }
