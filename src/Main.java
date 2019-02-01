37f4865a7682b47b5db080fbe48cafd79585b798
bojidar stanchev <bobiok641@gmail.com>
2019-02-01 23:24:14 +0200

    Added encrypting method and the program is rea

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,13 @@ public class Main {
         System.out.println("Enter a message to encrypt:");
         String message = input.next();
 
-        if (message.length() % 2 == 0) {
-            transformMessage(message);
-        } else {
-            System.out.println("The message should contain even number of letters!");
+        char[][] messageAsSeparatedPairs = transformMessage(message);
+        messageAsSeparatedPairs = encryptMessage(messageAsSeparatedPairs, matrix);
+
+        for (int i = 0; i < messageAsSeparatedPairs.length; i++) {
+            for (int j = 0; j < messageAsSeparatedPairs[0].length; j++) {
+                System.out.print(messageAsSeparatedPairs[i][j]);
+            }
         }
     }
 
@@ -64,7 +67,7 @@ public class Main {
                 Key = Key + currentLetterToAdd;
             letterIsNotUsed = true;
         }
-        System.out.println(Key);
+        //System.out.println(Key);
 
         int indexFromKey = 0;
         for (int i = 0; i < 5; i++) {
@@ -87,7 +90,7 @@ public class Main {
 
     public static char[][] transformMessage(String messageFromInput) {
         messageFromInput = messageFromInput + " ";
-        char[][] separatedPairs = new char[messageFromInput.length()][2];
+        char[][] separatedPairs = new char[messageFromInput.length()/2][2];
         int counter = 0;
         for (int i = 0; i < messageFromInput.length() - 1; i += 2) {
             if (messageFromInput.toCharArray()[i] == messageFromInput.toCharArray()[i + 1]) {
@@ -95,25 +98,83 @@ public class Main {
                 separatedPairs[counter][1] = 'x';
                 counter++;
                 i--;
-            }else if (i == messageFromInput.toCharArray().length - 2){
+            } else if (i == messageFromInput.toCharArray().length - 2) {
                 separatedPairs[counter][0] = messageFromInput.toCharArray()[i];
                 separatedPairs[counter][1] = 'x';
                 counter++;
-            }else{
+            } else {
                 separatedPairs[counter][0] = messageFromInput.toCharArray()[i];
                 separatedPairs[counter][1] = messageFromInput.toCharArray()[i + 1];
                 counter++;
             }
-
         }
 
         for (int i = 0; i < counter; i++) {
             for (int j = 0; j < 2; j++) {
-                System.out.print(separatedPairs[i][j]);
+                //System.out.print(separatedPairs[i][j]);
             }
-            System.out.println();
+            //System.out.println();
         }
 
+        return separatedPairs;
+    }
+
+    public static char[][] encryptMessage(char[][] separatedPairs, char[][] matrix) {
+        byte[][] pairsCoordinates = new byte[2][2];
+        byte[] swapCoordinates = new byte[2];
+        boolean isLastX;
+        boolean isLastY;
+        for (int i = 0; i < separatedPairs.length; i++) {
+            for (byte j = 0; j < separatedPairs[0].length; j++) {
+                for (byte x = 0; x < matrix.length; x++) {
+                    for (byte y = 0; y < matrix[0].length; y++) {
+                        if (separatedPairs[i][j] == matrix[x][y]) {
+                            pairsCoordinates[j][0] = x;
+                            pairsCoordinates[j][1] = y;
+                            //System.out.println(matrix[x][y]);
+                        }
+                    }
+                }
+            }
+            isLastX = false;
+            isLastY = false;
+            if (pairsCoordinates[0][0] == pairsCoordinates[1][0]) {
+                for (int j = 0; j < 2; j++) {
+                    if (pairsCoordinates[j][1] == 4) {
+                        pairsCoordinates[j][1] = 0;
+                        pairsCoordinates[Math.abs(j - 1)][1]++;
+                        isLastX = true;
+                    }
+                }
+                if (!isLastX) {
+                    pairsCoordinates[0][1]++;
+                    pairsCoordinates[1][1]++;
+                }
+
+            } else if (pairsCoordinates[0][1] == pairsCoordinates[1][1]) {
+                for (int j = 0; j < 2; j++) {
+                    if (pairsCoordinates[j][0] == 4) {
+                        pairsCoordinates[j][0] = 0;
+                        pairsCoordinates[Math.abs(j - 1)][0]++;
+                        isLastY = true;
+                    }
+                }
+                if (!isLastY) {
+                    pairsCoordinates[0][0]++;
+                    pairsCoordinates[1][0]++;
+                }
+            } else {
+                swapCoordinates[1] = pairsCoordinates[0][1];
+                pairsCoordinates[0][1] = pairsCoordinates[1][1];
+                pairsCoordinates[1][1] = swapCoordinates[1];
+            }
+
+            for (int j = 0; j < 2; j++) {
+                separatedPairs[i][j] = matrix[pairsCoordinates[j][0]][pairsCoordinates[j][1]];
+                //System.out.print(separatedPairs[i][j]);
+            }
+
+        }
         return separatedPairs;
     }
 }
